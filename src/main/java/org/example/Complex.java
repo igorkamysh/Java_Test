1abdb3206f46edc488c00ed32008dadb70d6a7bc
igorkamysh <igorkamysh@yandex.ru>
2023-06-02 07:56:22 +0300

    Commit

---
+++ src/main/java/org/example/Complex.java
@@ -0,0 +1,33 @@
+public class Complex {
+    int a;
+    int b;
+
+    public Complex(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Complex add(Complex num1, Complex num2) {
+        return new Complex(num1.a + num2.a, num1.b + num2.b);
+    }
+
+    public Complex sub(Complex num1, Complex num2) {
+        return new Complex(num1.a - num2.a, num1.b - num2.b);
+    }
+
+    public Complex mul(Complex num1, Complex num2) {
+        int a = num1.a;
+        int b = num1.b;
+        int c = num2.a;
+        int d = num2.b;
+        return new Complex(a * c - b * d, b * c + a * d);
+    }
+
+    public Complex div(Complex num1, Complex num2) {
+        int a = num1.a;
+        int b = num1.b;
+        int c = num2.a;
+        int d = num2.b;
+        return new Complex((a * c + b * d) / (c * c + d * d), (b * c - a * d) / (c * c + d * d));
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        Complex num1 = new Complex(2, 2);
+        Complex num2 = new Complex(3, 3);
+
+        System.out.println(num1.add(num1, num2));
+        System.out.println(num1.sub(num1, num2));
+        System.out.println(num1.mul(num1, num2));
+        System.out.println(num1.div(num1, num2));
+    }
+}
